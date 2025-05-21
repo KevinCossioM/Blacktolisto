3e83d0817844b9c3c4139c4a13421ae2a83054ae
Kevin Cossio M <198007979+KevinCossioM@users.noreply.github.com>
2025-05-20 20:11:04 -0500

    Inicio de la aventura

---
+++ blacktolisto/src/main/java/com/mycompany/blacktolisto/Blacktolisto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blacktolisto;
+
+/**
+ *
+ * @author ASRock
+ */
+public class Blacktolisto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    
+    }
+}

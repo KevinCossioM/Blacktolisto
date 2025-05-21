edaf9df2fb78e68c6abc022fbfd6795d873da44e
Kevin Cossio M <198007979+KevinCossioM@users.noreply.github.com>
2025-05-20 20:22:23 -0500

    Agregando saludo

--- blacktolisto/src/main/java/com/mycompany/blacktolisto/Blacktolisto.java
+++ blacktolisto/src/main/java/com/mycompany/blacktolisto/Blacktolisto.java
@@ -11,7 +11,12 @@ package com.mycompany.blacktolisto;
 public class Blacktolisto {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Not today my fella!");
     
+    
+    //Cambios hechos por prueba
+     var mensaje = "No hoy compañero!";
+        System.out.println(mensaje);
+        
     }
 }
